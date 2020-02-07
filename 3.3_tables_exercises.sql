a9bc8fecbb441f626a65b726b577c6f63a84bc1c
Your Name <the.jeremy.cobb@gmail.com>
2020-02-07 15:39:06 -0600

    added some fixes

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -10,7 +10,7 @@ DESCRIBE employees;
 
 ### 6 emp_no, gender
 
-### 7 first_name, lasrt_name
+### 7 first_name, last_name, gender
 
 ### 8 birth_date, hire_date
 
@@ -25,4 +25,20 @@ SELECT * FROM employees_with_departments;
 SELECT * FROM dept_manager;
 
 SHOW CREATE TABLE dept_manager;
+
+SELECT * FROM salaries;
+
+-/* - CREATE TABLE `dept_manager` (
+  `emp_no` INT(11) NOT NULL,
+  `dept_no` CHAR(4) NOT NULL,
+  `from_date` DATE NOT NULL,
+  `to_date` DATE NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ) 
+--   ENGINE=INNODB DEFAULT CHARSET=latin1  */
+
+
+
 
