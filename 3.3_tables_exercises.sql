c14856cd807f79a1c2463a4c1e98e6fbb90cb925
Your Name <the.jeremy.cobb@gmail.com>
2020-02-07 14:56:26 -0600

    added correct file

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+SHOW TABLES;
+
+SELECT * FROM employees;
+
+DESCRIBE employees;
+
+### 5 int, date, varchar, enum
+
+### 6 emp_no, gender
+
+### 7 first_name, lasrt_name
+
+### 8 birth_date, hire_date
+
+SELECT * FROM departments;
+
+DESCRIBE departments;
+
+### 9 the relationship is the employees_with_departments table 
+
+SELECT * FROM employees_with_departments;
+
+SELECT * FROM dept_manager;
+
+SHOW CREATE TABLE dept_manager;
+
