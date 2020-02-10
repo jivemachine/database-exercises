87daade51b9379bc5f2627101582597a172885b6
Your Name <the.jeremy.cobb@gmail.com>
2020-02-10 11:30:47 -0600

    updated exercise

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -1,8 +1,11 @@
 USE employees;
 
 # 2
-SELECT first_name FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE
+gender = 'M' AND 
+(first_name LIKE 'Irena' OR
+first_name LIKE 'Vidya' OR
+first_name LIKE 'Maya');
 
 # 3
 SELECT last_name FROM employees
@@ -19,5 +22,22 @@ WHERE birth_date LIKE "%-12-25";
 #6 
 SELECT * FROM employees
 WHERE last_name LIKE "%q%";
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%' 
+OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees
+WHERE hire_date BETWEEN "1990-01-01" AND "1999-12-31"
+AND birth_date LIKE '%-12-25';
+
+SELECT * FROM employees
+WHERE last_name NOT LIKE "%qu%"
+AND last_name LIKE "%q%";
+
+
 
 
